6f852c807a058a67388d766016c38e078f3505de
Jefferson Shibuya <jeffersonshibuya@yahoo.com.br>
2021-06-29 09:34:50 -0300

    Aula 3 - Completa

---
+++ src/routes.tsx
@@ -0,0 +1,18 @@
+
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { NewRoom } from './pages/NewRoom';
+import Room from './pages/Room';
+
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms/new" component={NewRoom} />
+        <Route path="/rooms/:id" component={Room} />
+      </Switch>
+    </BrowserRouter>
+  )
+}
